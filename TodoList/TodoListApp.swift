f72c84e9562682c95182fb86ac2abd4e507a6b84
wanghl <wanghl@pingpongx.com>
2026-02-26 14:42:37 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 王海龙 on 2026/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
